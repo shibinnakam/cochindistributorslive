037f6bad468ca9bbde69917397b8c39d99954068
code-e-star <shibinsajip@gmail.com>
2026-02-24 13:49:51 +0530

    product updated

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -230,7 +230,7 @@ export default {
         el.scrollIntoView({ behavior: 'smooth' });
       }
     },
-    orderProduct(product) {
+    orderProduct() {
       // Redirect to user page or login if not logged in
       const token = localStorage.getItem("token");
       if (!token) {
